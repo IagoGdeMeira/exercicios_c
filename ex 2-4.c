820598bc535d582a168cd773054d564a25dc1c3f
Iago Gonçalves de Meira <93204133+IagoGdeMeira@users.noreply.github.com>
2023-02-24 13:47:36 -0300

    Create ex 2-4.c

---
+++ ex 2-4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    float razao, termo_1, total;
+    
+    scanf("%f", &razao);
+    scanf("%f", &termo_1);
+    
+    total = termo_1;
+    
+    for(int i = 2; i <= 5; i++) {
+        total = total * razao;
+    }
+    
+    printf("TOTAL: %.2f", total);
+}
