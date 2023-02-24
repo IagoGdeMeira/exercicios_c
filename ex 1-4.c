60fc4725495371575674039ea3e8554701575b41
Iago Gonçalves de Meira <93204133+IagoGdeMeira@users.noreply.github.com>
2023-02-24 10:41:47 -0300

    Create ex 1-4.c

---
+++ ex 1-4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int x, y, soma;
+    
+    scanf("%d", &x);
+    scanf("%d", &y);
+    soma = x + y;
+    
+    printf("%d + %d = %d", x, y, soma);
+    
+    return 0;
+}
