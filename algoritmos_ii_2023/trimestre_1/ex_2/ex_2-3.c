581ba2fb954d62100f3fdc512c013f9f0b7f4717
Iago Gonçalves de Meira <93204133+IagoGdeMeira@users.noreply.github.com>
2023-02-28 13:55:26 -0300

    Update ex_2-3.c

--- algoritmos_ii_2023/trimestre_1/ex_2/ex_2-3.c
+++ algoritmos_ii_2023/trimestre_1/ex_2/ex_2-3.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 int main() {
-    float razao, termo_1, total;
+    float razao, termo_1, termo_10;
     
     scanf("%f", &razao);
     scanf("%f", &termo_1);
     
-    total = termo_1;
+    termo_10 = termo_1;
     
-    for(int i = 2; i <= 5; i++) {
-        total += razao;
+    for(int i = 2; i <= 10; i++) {
+        termo_10 += razao;
     }
     
-    printf("TOTAL: %.2f", total);
+    printf("TERMO 10: %.2f", termo_10);
     
     return 0;
 }
