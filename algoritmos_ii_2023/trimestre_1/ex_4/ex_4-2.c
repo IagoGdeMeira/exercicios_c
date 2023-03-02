33211566b7538f4f0fdae58bf2371411799f0aab
Iago Gonçalves de Meira <93204133+IagoGdeMeira@users.noreply.github.com>
2023-03-02 20:53:00 -0300

    Create ex_4-2.c

---
+++ algoritmos_ii_2023/trimestre_1/ex_4/ex_4-2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main () {
+    int num[15];
+    
+    for(int i = 0; i < 15; i++) {
+        printf("Informe o %dº número: ", i+1);
+        scanf("%d", &num[i]);
+    }
+    
+    for(int i = 0; i < 15; i++) {
+        if(num[i] % 2 == 0) {
+            printf("%d é um número par.\n", num[i]);
+        } else {
+            printf("%d é um número ímpar.\n", num[i]);
+        }
+    }
+    
+    return 0;
+}
