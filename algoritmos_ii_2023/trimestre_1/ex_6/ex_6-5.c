7636b0e034877e8982a16feb1ca713097a62fb40
Iago Gonçalves de Meira <93204133+IagoGdeMeira@users.noreply.github.com>
2023-03-09 21:15:31 -0300

    Update ex_6-5.c

--- algoritmos_ii_2023/trimestre_1/ex_6/ex_6-5.c
+++ algoritmos_ii_2023/trimestre_1/ex_6/ex_6-5.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int main() {
-    int vet_int[3];
+    int vet_int[30];
     
-    for(int i = 0; i < 3; i++) {
+    for(int i = 0; i < 30; i++) {
         printf("Informe o %2dº inteiro: ", i+1);
         scanf("%d", &vet_int[i]);
     }
@@ -11,7 +11,7 @@ int main() {
     for(int i = 1; i <= 3; i++)
         printf("\n");
         
-    for(int i = 2; i >= 0; i--)
+    for(int i = 29; i >= 0; i--)
         printf("%2dº valor do conjunto: %d\n", i, vet_int[i]);
     
     return 0;
