4817ff6db4788d70a510b7e2883b40efb88e4bb2
Iago Gonçalves de Meira <93204133+IagoGdeMeira@users.noreply.github.com>
2023-03-13 15:28:45 -0300

    Create ex_8-2.c

---
+++ algoritmos_ii_2023/trimestre_1/ex_8/ex_8-2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+struct cliente {
+    char nomeCliente[50];
+    int codAgencia;
+    char dataNascimento[10];
+    float saldoAtual;
+    char situacao;
+};
+
+int main() {
+    struct cliente cliente1;
+    
+    return 0;
+}
