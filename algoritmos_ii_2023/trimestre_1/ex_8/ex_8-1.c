14e3d0d7a95919b01541f1e291a887762b077744
Iago Gonçalves de Meira <93204133+IagoGdeMeira@users.noreply.github.com>
2023-03-13 15:28:21 -0300

    Update ex_8-1.c

--- algoritmos_ii_2023/trimestre_1/ex_8/ex_8-1.c
+++ algoritmos_ii_2023/trimestre_1/ex_8/ex_8-1.c
@@ -13,13 +13,5 @@ struct veiculo {
 int main() {
     struct veiculo carro1;
     
-    carro1.fabricante = "Munhoz&Mariano's";
-    carro1.modelo = "Camaro";
-    carro1.ano = 2017;
-    carro1.placa = "GSH2945";
-    carro1.tipo_combustivel = 'G';
-    carro1.preco = 512284.29;
-    carro1.ipva = 20491.37;
-    
     return 0;
 }
