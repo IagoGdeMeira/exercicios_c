638c8af10c3235a0fab9df711b11a854e50b9e1c
Iago Gonçalves de Meira <93204133+IagoGdeMeira@users.noreply.github.com>
2023-03-13 16:01:04 -0300

    Update ex_8-2.c

--- algoritmos_ii_2023/trimestre_1/ex_8/ex_8-2.c
+++ algoritmos_ii_2023/trimestre_1/ex_8/ex_8-2.c
@@ -1,15 +1,26 @@
 #include <stdio.h>
 
 struct cliente {
-    char nomeCliente[50];
+    char nomeCliente[51];
     int codAgencia;
-    char dataNascimento[10];
+    char dataNascimento[11];
     float saldoAtual;
     char situacao;
 };
 
 int main() {
     struct cliente cliente1;
+    
+    printf("Nome do cliente: ");
+    scanf("%s", cliente1.nomeCliente);
+    printf("Código da agência: ");
+    scanf("%d", &cliente1.codAgencia);
+    printf("Data de nascimento do cliente: ");
+    scanf("%s", cliente1.dataNascimento);
+    printf("Saldo atual do cliente: ");
+    scanf("%f", &cliente1.saldoAtual);
+    printf("Situação do cliente: ");
+    scanf("%s", &cliente1.situacao);
     
     return 0;
 }
